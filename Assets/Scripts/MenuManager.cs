94d9c14f08e9cf9006feea83d9cd812c71f1a395
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2022-04-08 14:15:32 -0300

    [FINISHED] EndScreen

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject[] toggleObject;
+    public void ButtonHit()
+    {
+        for (int i = 0; i < toggleObject.Length; i++)
+        {
+            bool currentState = true;
+            if (this.toggleObject[i].activeSelf)
+            {
+                this.toggleObject[i].SetActive(!currentState);
+            }
+
+            else if (!this.toggleObject[i].activeSelf)
+            {
+                this.toggleObject[i].SetActive(currentState);
+            }
+        }
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);
+    }
+}
